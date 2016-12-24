5b71f2982eec0991a93ae3ccf422433783c46159
rogelio morrell <rogelio morrell>
2016-12-24 14:23:49 -0500

    init

---
+++ DSS.UareU.Web.Api.Service/Controllers/ApiModule.cs
@@ -0,0 +1,48 @@
+﻿using DPUruNet;
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSS.UareU.Web.Api.Service.Controllers
+{
+    public class ApiModule : NancyModule
+    {
+        public ApiModule() : base("/api")
+        {
+            Get["/health"] = parameters =>
+            {
+                return "OK";
+            };
+
+            Get["/info", true] = async (parameters, ct) =>
+            {
+                var readers = ReaderCollection.GetReaders();
+                var tcs = new TaskCompletionSource<Reader.ReaderDescription>();
+
+                if (readers.Count > 0)
+                {
+                    var reader = readers.FirstOrDefault();
+                        if (reader.Open(Constants.CapturePriority.DP_PRIORITY_COOPERATIVE) == Constants.ResultCode.DP_SUCCESS)
+                        {
+                            tcs.SetResult(reader.Description);
+                        } else
+                        {
+                        tcs.SetResult(null);
+                        }
+                        reader.Dispose();
+
+                } else
+                {
+                    tcs.SetCanceled();
+                }
+
+                return await tcs.Task;
+
+            };
+
+        }
+    }
+}
---
+++ WebHost/Program.cs
@@ -0,0 +1,24 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostConfiguration config = new HostConfiguration();
+            config.UrlReservations.CreateAutomatically = true;
+            using (var host = new NancyHost(config, new Uri("http://localhost:1234")))
+            {
+                host.Start();
+                Console.WriteLine("Running on http://localhost:1234");
+                Console.ReadLine();
+            }
+        }
+    }
+}
