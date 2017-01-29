bbc247dcbb5a40682c28760181a13bb940df3132
Rogelio Morrell <molekilla@gmail.com>
2017-01-29 13:11:44 -0500

    playing with ws

--- ClientHost/Program.cs
+++ ClientHost/Program.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.Owin.Hosting;
+﻿using DSS.UareU.Web.Api.Client.Controllers.V1;
+using Microsoft.Owin.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebSocketSharp.Server;
 
 namespace ClientHost
 {
@@ -11,6 +13,9 @@ namespace ClientHost
     {
         static void Main(string[] args)
         {
+            var wssv = new WebSocketServer("ws://localhost:8881");
+            wssv.AddWebSocketService<PingWebSocketController>("/ping");
+            wssv.Start();
             var url = "http://localhost:8088";
 
             using (WebApp.Start<Startup>(url))
---
+++ DSS.UareU.Web.Api.Client/Controllers/V1/CaptureWebSocketController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace DSS.UareU.Web.Api.Client.Controllers.V1
+{
+    public class CaptureWebSocketController : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Send("OK");
+        }
+    }
+}
